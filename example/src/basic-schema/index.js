0f516aad7131d81673a7276cfab95099f5a605e2
Cornel Stefanache <cornel.stefanache@gmail.com>
2020-05-12 07:45:36 +0300

    code loading through !!raw-loader

--- example/src/basic-schema/index.js
+++ example/src/basic-schema/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import schema from './basic-schema.json'
 import { SchemaForm } from '@ascentcore/react-schema-form'
 
-export function BasicSchemaExample() {
+export default function BasicSchemaExample() {
 
     function onValid(data) {
         console.log(data)
@@ -15,24 +15,3 @@ export function BasicSchemaExample() {
     return (<SchemaForm schema={schema} onValid={onValid} data={data} />)
 
 }
-
-export const BasicSchemaExampleJson = schema
-export const BasicSchemaExampleCode = `
-import React from 'react'
-import schema from './basic-schema.json'
-import { SchemaForm } from '@ascentcore/react-schema-form'
-
-export function BasicSchema() {
-
-    function onValid(data) {
-        console.log(data)
-    }
-
-    const data = {
-        firstName: 'test'
-    }
-
-    return (<SchemaForm schema={schema} onValid={onValid} data={data} />)
-
-}
-`
--- example/src/custom-wrapper/index.js
+++ example/src/custom-wrapper/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import schema from './basic-schema.json'
+import schema from './custom-wrapper-schema.json'
 import { SchemaForm } from '@ascentcore/react-schema-form'
 
 function CustomWrapper({ property, error, children }) {
@@ -10,7 +10,7 @@ function CustomWrapper({ property, error, children }) {
     </div>)
 }
 
-export function CustomWrapperExample() {
+export default function CustomWrapperExample() {
 
     function onValid(data) {
         console.log(data)
@@ -32,42 +32,3 @@ export function CustomWrapperExample() {
         </div>)
 
 }
-
-
-export const CustomWrapperExampleJson = schema
-export const CustomWrapperExampleCode = `
-import React from 'react'
-import schema from './basic-schema.json'
-import { SchemaForm } from '@ascentcore/react-schema-form'
-
-function CustomWrapper({ property, error, children }) {
-    return (<div className="column col-4">
-        <div><b>{property.title}</b></div>
-        {children}
-        <div style={{ 'color': '#FF0000' }}>{error && error[0].keyword}</div>
-    </div>)
-}
-
-export function CustomWrapperExample() {
-
-    function onValid(data) {
-        console.log(data)
-    }
-
-    const data = {
-        firstName: 'test'
-    }
-
-    return (
-        <div className="container">
-            <div className="columns">
-                <SchemaForm
-                    schema={schema}
-                    onValid={onValid}
-                    wrapper={CustomWrapper}
-                    data={data} />
-            </div>
-        </div>)
-
-}
-`
