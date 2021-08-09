6d283d1149bb3d515c5d830538d1d0d776b47633
Cornel Stefanache <cornel.stefanache@gmail.com>
2021-08-09 11:14:17 +0300

    added samples

---
+++ docs/samples/precache-manifest.14d0a5f6b129b79b6c32de8fadacbdca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a04f4d6f0a1026277319ddfeb2e8cf",
+    "url": "/@ascentcore/react-schema-form/index.html"
+  },
+  {
+    "revision": "4257f95308a5c4892837",
+    "url": "/@ascentcore/react-schema-form/static/css/2.90c11924.chunk.css"
+  },
+  {
+    "revision": "e89912dda5d7c904137a",
+    "url": "/@ascentcore/react-schema-form/static/css/main.894a1f24.chunk.css"
+  },
+  {
+    "revision": "4257f95308a5c4892837",
+    "url": "/@ascentcore/react-schema-form/static/js/2.a8893180.chunk.js"
+  },
+  {
+    "revision": "6490cc68a53bf8fe06282eb0d74192cb",
+    "url": "/@ascentcore/react-schema-form/static/js/2.a8893180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e89912dda5d7c904137a",
+    "url": "/@ascentcore/react-schema-form/static/js/main.484af1d0.chunk.js"
+  },
+  {
+    "revision": "eb93f09b93734a59c69f",
+    "url": "/@ascentcore/react-schema-form/static/js/runtime-main.238d6e86.js"
+  }
+]);
